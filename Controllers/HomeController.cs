d2853f652739cf47fe47df258391466062933574
micrychelik <mikhailyaroshevitch@yandex.ru>
2023-07-11 14:31:22 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NevaNetCore.Models;
+using System.Diagnostics;
+using NevaNetCore;
+using System.Text.Json;
+using System;
+using Neva.Drivers.Fptr;
+
+namespace NevaNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        ConnectionSettings connection = new ConnectionSettings();
+        IFptr fptr = new Fptr();
+        private readonly ILogger<HomeController> _logger;
+        
+        public HomeController(ILogger<HomeController> logger)
+        {
+             //C:\Program Files\NevaDriver
+            _logger = logger;
+            //IFptr fptr = new Fptr(getApplicationContext());
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            IFptr fptr = new Fptr(); //C:\Program Files\NevaDriver
+            int p = 10;
+            connection.Version_driver = fptr.version();
+            return View(connection);
+
+        }
+        [HttpPost]
+        public IActionResult Index(ConnectionSettings model)
+        {
+            fptr.setSettings(JsonSerializer.Serialize(connection));
+            if (fptr.printText() < 0)
+            {
+                connection.Connection_status = fptr.errorCode().ToString();
+                connection.Version_driver = "dasd";
+            }
+            return View(connection);
+        }
+        public IActionResult CONNECT()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ConnectionSettings.cs
@@ -0,0 +1,37 @@
+﻿namespace NevaNetCore.Models
+{
+    public class ConnectionSettings
+    {
+        public String LIBFPTR_SETTING_PORT { get; set; }
+        public String LIBFPTR_SETTING_BAUDRATE { get; set; }
+        public String LIBFPTR_SETTING_BITS { get; set; }
+        public String LIBFPTR_SETTING_SB { get; set; }
+        public String LIBFPTR_SETTING_PARITY { get; set; }
+        public String LIBFPTR_SETTING_COM_FILE { get; set; }
+
+        public String LIBFPTR_SETTING_IPADDRESS { get; set; }
+
+        public String Connection_status { get; set; }
+
+        public String Version_driver { get; set; }
+        public ConnectionSettings()
+        {
+            LIBFPTR_SETTING_PORT = "LIBFPTR_PORT_COM";
+            LIBFPTR_SETTING_BAUDRATE = "LIBFPTR_PORT_BR_115200";
+            LIBFPTR_SETTING_BITS = "LIBFPTR_SETTING_BITS_7";
+            LIBFPTR_SETTING_SB = "LIBFPTR_SETTING_SB_1";
+            LIBFPTR_SETTING_PARITY = "LIBFPTR_SETTING_PARITY_NO";
+            LIBFPTR_SETTING_COM_FILE = "COM1";
+            LIBFPTR_SETTING_IPADDRESS = "192.168.1.10";
+            Connection_status = " ";
+            Version_driver = " ";
+        }
+    }
+}
+//public String LIBFPTR_SETTING_PORT = "LIBFPTR_PORT_COM";
+//public String LIBFPTR_SETTING_BAUDRATE = "LIBFPTR_PORT_BR_115200";
+//public String LIBFPTR_SETTING_BITS = "LIBFPTR_SETTING_BITS_7";
+//public String LIBFPTR_SETTING_SB = "LIBFPTR_SETTING_SB_1";
+//public String LIBFPTR_SETTING_PARITY = "LIBFPTR_SETTING_PARITY_NO";
+//public String LIBFPTR_SETTING_COM_FILE = "COM1";
+//public String LIBFPTR_SETTING_IPADDRESS = "192.168.1.10";
